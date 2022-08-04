a725d2724a3b45121020fbd0fb2924c9660ac059
mlbird1 <mlbird1@sheffield.ac.uk>
2022-08-04 12:59:35 +0100

    Added batterys to all the cars

--- engine/model/palindrome.py
+++ engine/model/palindrome.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from engine.sternman_engine import SternmanEngine
+from battery.spindler_battery import SpindlerBattery
 
 
 class Palindrome(SternmanEngine):
@@ -10,3 +11,13 @@ class Palindrome(SternmanEngine):
             return True
         else:
             return False
+
+
+class Calliope(SpindlerBattery):
+   def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
+
