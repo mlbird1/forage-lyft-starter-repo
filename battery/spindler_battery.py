34dcdb9067ab0710259b61ccd657451c6639a260
mlbird1 <mlbird1@sheffield.ac.uk>
2022-08-04 12:48:27 +0100

    Added battery files

---
+++ battery/spindler_battery.py
@@ -0,0 +1,11 @@
+from abc import ABC
+from datetime import datetime
+from car import Car
+
+class SpindlerBattery(Car, ABC):
+    def __init__(self, last_service_date, current_date):
+        self.current_date = datetime.today().date()
+        super().__init__(last_service_date)
+
+    def battery_should_be_serviced(self):
+        return self.current_date - self.last_service_date > 2
