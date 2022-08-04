1e43baa96608f79460cffb90ba4e498a7e103112
mlbird1 <mlbird1@sheffield.ac.uk>
2022-08-04 16:57:25 +0100

    Added temp test file

---
+++ test_car_task3.py
@@ -0,0 +1,46 @@
+import unittest
+from datetime import datetime
+
+from engine.model.calliope import Calliope
+from engine.model.glissade import Glissade
+from engine.model.palindrome import Palindrome
+from engine.model.rorschach import Rorschach
+from engine.model.thovex import Thovex
+
+class TestCalliope(unittest.TestCase):
+    def test_battery_should_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 3)
+        current_mileage = 0
+        last_service_mileage = 0
+
+        car = Calliope(last_service_date, current_mileage, last_service_mileage)
+        self.assertTrue(car.needs_service())
+
+    def test_battery_should_not_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 1)
+        current_mileage = 0
+        last_service_mileage = 0
+
+        car = Calliope(last_service_date, current_mileage, last_service_mileage)
+        self.assertFalse(car.needs_service())
+
+    def test_engine_should_be_serviced(self):
+        last_service_date = datetime.today().date()
+        current_mileage = 30001
+        last_service_mileage = 0
+
+        car = Calliope(last_service_date, current_mileage, last_service_mileage)
+        self.assertTrue(car.needs_service())
+
+    def test_engine_should_not_be_serviced(self):
+        last_service_date = datetime.today().date()
+        current_mileage = 30000
+        last_service_mileage = 0
+
+        car = Calliope(last_service_date, current_mileage, last_service_mileage)
+        self.assertFalse(car.needs_service())
+
+if __name__ == '__main__':
+    unittest.main()
